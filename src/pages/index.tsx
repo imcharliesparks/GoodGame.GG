5a63b41cf698ce994967718d481552029f6440c0
Charlie Sparks <sparksfullstack@gmail.com>
2023-05-15 16:15:29 -0400

    Charlie | Made it pretty uwu

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,13 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import { Inter } from 'next/font/google'
+import { useClerk } from '@clerk/nextjs'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+	const { signOut } = useClerk()
+
 	return (
 		<main className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}>
 			<div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
@@ -14,33 +17,32 @@ export default function Home() {
 				</p>
 				<div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
 					<Link
-						className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-						href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+						className="flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+						href="https://sparksfullstack.io"
 						target="_blank"
 						rel="noopener noreferrer"
 					>
-						By <Image src="/vercel.svg" alt="Vercel Logo" className="dark:invert" width={100} height={24} priority />
+						By{' '}
+						<Image src="/resources/sfs-logo-full.svg" alt="The Sparks Full-Stack logo" width={290} height={72.5} priority />
 					</Link>
 				</div>
 			</div>
 
-			<div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-				<Image
-					className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-					src="/next.svg"
-					alt="Next.js Logo"
-					width={180}
-					height={37}
-					priority
-				/>
-			</div>
+			<Link
+				className="flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+				href="https://sparksfullstack.io"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				<div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
+					<Image src="/resources/sfs-icon.svg" alt="The Sparks Full-Stack logo" width={180} height={37} priority />
+				</div>
+			</Link>
 
 			<div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
 				<Link
 					href="/app/search-games"
 					className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-					target="_blank"
-					rel="noopener noreferrer"
 				>
 					<h2 className={`mb-3 text-2xl font-semibold`}>
 						Search Games{' '}
@@ -54,8 +56,6 @@ export default function Home() {
 				<Link
 					href="/app/my-collection"
 					className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-					target="_blank"
-					rel="noopener noreferrer"
 				>
 					<h2 className={`mb-3 text-2xl font-semibold`}>
 						View Collection{' '}
@@ -69,8 +69,6 @@ export default function Home() {
 				<Link
 					href="/auth/sign-in"
 					className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-					target="_blank"
-					rel="noopener noreferrer"
 				>
 					<h2 className={`mb-3 text-2xl font-semibold`}>
 						Get Auth'd{' '}
@@ -80,6 +78,18 @@ export default function Home() {
 					</h2>
 					<p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Sign In/Up with Clerk</p>
 				</Link>
+				<button
+					onClick={() => signOut}
+					className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+				>
+					<h2 className={`mb-3 text-2xl font-semibold`}>
+						Sign out{' '}
+						<span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+							-&gt;
+						</span>
+					</h2>
+					<p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Sign Out with Clerk</p>
+				</button>
 			</div>
 		</main>
 	)
