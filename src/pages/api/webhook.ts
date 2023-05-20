2688e8533827ba28df28a05231165c475de2098a
Charlie Sparks <sparksfullstack@gmail.com>
2023-05-20 17:24:54 -0400

    Charlie | Pushing to test web hook

--- src/pages/api/webhook.ts
+++ src/pages/api/webhook.ts
@@ -4,7 +4,7 @@ import { NextApiRequest, NextApiResponse } from 'next'
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 	if (req.method === 'POST') {
 		// The payload of the webhook is stored in req.body
-		const payload = req.body.evt as WebhookEvent
+		const payload = req.body
 
 		// You can now do something with the payload
 		console.log(payload)
