cafd44d7ac9e6eb6b4932b5a2838bc385d0136a1
Charlie Sparks <sparksfullstack@gmail.com>
2024-02-18 12:27:32 -0500

    Charlie | Added has game computed property to state

--- src/state/gamesState.tsx
+++ src/state/gamesState.tsx
@@ -8,12 +8,13 @@ export interface GamesState {
 	getGamesFromCurrentList: () => StoredGame[]
 }
 
-export const useGamesStore = create<GamesState>((set) => ({
+export const useGamesStore = create<GamesState>((set, get) => ({
 	currentlySelectedGame: undefined,
 	// TODO: Think of a better way to do this. This will only be a MobyGame when on the search page
 	setCurrentlySelectedGame: (game: StoredGame | MobyGame) => {
 		return set({ currentlySelectedGame: game })
 	},
+	// TODO: Memoize both of these below
 	getGamesFromCurrentList: () => {
 		const userListsState = useUserListsStore.getState()
 		const currentlySelectedList = userListsState.currentlySelectedList
@@ -22,6 +23,21 @@ export const useGamesStore = create<GamesState>((set) => ({
 			games.push(userListsState.lists[currentlySelectedList][game_id])
 		}
 		return games
+	},
+	// TODO: Memoize and implement instead of the `hasGame` everywhere
+	getUserHasGame: () => {
+		const currentlySelectedGame = get().currentlySelectedGame
+		const lists = useUserListsStore.getState().lists
+
+		if (!currentlySelectedGame) return false
+
+		for (let listName in lists) {
+			if (lists[listName][currentlySelectedGame?.game_id]) {
+				return true
+			}
+		}
+
+		return false
 	}
 }))
 
