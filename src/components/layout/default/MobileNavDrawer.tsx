1066ee4bddb9e53fb72c22218eb80d203ee14962
Charlie Sparks <sparksfullstack@gmail.com>
2024-03-05 11:56:03 -0500

    Charlie | Dead packages removed; icons updated for mobile nav drawer

--- src/components/layout/default/MobileNavDrawer.tsx
+++ src/components/layout/default/MobileNavDrawer.tsx
@@ -69,6 +69,44 @@ const MobileNavDrawer = ({ open, closeDrawer }: MobileNavDrawerProps) => {
 						</ListItemPrefix>
 						My Collection
 					</ListItem>
+					<ListItem onClick={() => router.push('/app/user/lists/Wishlist')}>
+						<ListItemPrefix>
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke-width="1.5"
+								stroke="currentColor"
+								className="w-6 h-6"
+							>
+								<path
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
+								/>
+							</svg>
+						</ListItemPrefix>
+						My Wishlist
+					</ListItem>
+					<ListItem onClick={() => router.push('/app/user/lists/Backlog')}>
+						<ListItemPrefix>
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke-width="1.5"
+								stroke="currentColor"
+								className="w-6 h-6"
+							>
+								<path
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25"
+								/>
+							</svg>
+						</ListItemPrefix>
+						My Backlog
+					</ListItem>
 					<ListItem onClick={() => router.push('/app/user/lists')}>
 						<ListItemPrefix>
 							<svg
